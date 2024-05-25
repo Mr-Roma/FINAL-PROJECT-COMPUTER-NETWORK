d542f5d6ac756eee6b37d1f9c20e6f456cde8651
Romario Marcal <romariomarcal11@gmail.com>
2024-05-25 17:28:21 +0900

    Fix the Single Thread Server

--- server.py
+++ server.py
@@ -1,50 +1,65 @@
 import socket
 import os
 
-def handle_client(client_socket):
-    """Handles a client request."""
+def handle_client(client_socket, addr):
     request = client_socket.recv(1024).decode()
     headers = request.split('\n')
-    filename = headers[0].split()[1]
+    if headers and headers[0]:
+        parts = headers[0].split()
+        if len(parts) > 1:
+            filename = parts[1]
+            if filename == '/':
+                filename = '/index.html'
 
-    if filename == '/':
-        filename = '/index.html'
+            try:
+                file_type = filename.split('.')[-1]
+                if file_type in ["jpg", "gif", "png", "webp", "ico"]:
+                    fin = open('.' + filename, 'rb')
+                    content = fin.read()
+                    fin.close()
+                    response = b'HTTP/1.1 200 OK\nContent-Type: image/' + file_type.encode() + b'\n\n' + content
+                elif file_type in ["html", "css", "js"]:
+                    fin = open('.' + filename, 'r')
+                    content = fin.read()
+                    fin.close()
+                    response = 'HTTP/1.1 200 OK\nContent-Type: text/' + file_type + '\n\n' + content
+                    response = response.encode()
+                else:
+                    fin = open('.' + filename, 'rb')  # Open in binary mode
+                    content = fin.read()
+                    fin.close()
+                    response = b'HTTP/1.1 200 OK\n\n' + content  # Response is bytes
 
-    try:
-        fin = open('htdocs' + filename)
-        content = fin.read()
-        fin.close()
+            except FileNotFoundError:
+                response = 'HTTP/1.1 404 NOT FOUND\n\nFile Not Found'
+                response = response.encode()  # Encode here
+        else:
+            print("Invalid HTTP request line:", headers[0])
+            response = 'HTTP/1.1 400 BAD REQUEST\n\nInvalid HTTP request line'
+            response = response.encode()  # Encode here
+    else:
+        print("Empty request received")
+        response = 'HTTP/1.1 400 BAD REQUEST\n\nEmpty request received'
+        response = response.encode()  # Encode here
 
-        response = 'HTTP/1.1 200 OK\n\n' + content
-    except FileNotFoundError:
-        response = 'HTTP/1.1 404 NOT FOUND\n\nFile Not Found'
-
-    client_socket.send(response.encode())
+    client_socket.send(response)
     client_socket.close()
 
 def server():
+    ip_address = socket.gethostbyname(socket.gethostname())
+    port = 1234
     """Creates a server that handles one HTTP request at a time."""
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((socket.gethostname(), 1234))
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((ip_address, port))
     server_socket.listen(5)
+
+    print(f"Server ready...\nServer is running on port http://{ip_address}:{port}")
 
     while True:
         client_socket, addr = server_socket.accept()
         print(f"Accepted connection from: {addr[0]}:{addr[1]}")
-        handle_client(client_socket)
+        handle_client(client_socket, addr)  # pass addr to handle_client
 
 if __name__ == "__main__":
     server()
-    
-    
-    
-#To print the ip address of the host that are used in this server 
-
-# def print_ip_address():
-#     """Prints the IP address of the machine."""
-#     hostname = socket.gethostname()
-#     ip_address = socket.gethostbyname(hostname)
-#     print(f"IP Address: {ip_address}")
-
-# if __name__ == "__main__":
-#     print_ip_address()
